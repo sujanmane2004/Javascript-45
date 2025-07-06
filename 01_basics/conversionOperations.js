025aca94fa041bfed13f0b60b65ecd778f90e496
sujanmane2004 <sujanmane11@gmail.com>
2025-07-06 10:22:32 +0000

    conversion of datatypes

---
+++ 01_basics/conversionOperations.js
@@ -0,0 +1,19 @@
+let score = "33abc"
+//let score = "33"
+
+//console.log(typeof score);
+
+let valueInNumber = Number(score);
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+// "33" => 33
+//"33abc" => NaN
+
+let isLogin = 1
+let booleanLogin=Boolean(isLogin);
+console.log(booleanLogin);
+
+//1 => true , 0=> false
+// "" => false
+// "sujan" => true
