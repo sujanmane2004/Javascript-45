48f456a6a484f3656ee9933b61c6da298890b0c4
sujanmane2004 <sujanmane11@gmail.com>
2025-07-06 10:02:26 +0000

    datatypes

---
+++ 01_basics/dataTypes.js
@@ -0,0 +1,24 @@
+//alert(3+3)  // we are using nodejs not browser
+
+console.log(3+6);
+
+console.log("Sujan");
+
+
+let name = "sujan"
+let age = 21
+let Login = false
+
+// number
+//string
+//boolean (true , false)
+// null => standalone value
+// undefined => not defined 
+// symbol (use for uniqueness)
+
+
+//object
+
+console.log(typeof undefined); // undefined 
+console.log(typeof null);  // object
+
